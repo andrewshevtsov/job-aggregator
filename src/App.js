c07d555acc108f434311a01c82f483e54e62d85d
andrewshevtsov <andrewshevtsov@inbox.ru>
2017-12-24 14:47:50 +0300

    pagination and onListScroll handler was added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,56 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ListItem from './ListItem';
-import axios from 'axios';
 
 class App extends Component {
 
-    state = {}
+    state = {
+        data: {
+            items: []
+        },
+        isListLoading: false
+    }
 
-    componentDidMount() {
-        axios.get('https://api.hh.ru/vacancies')
-            .then((result) => {
-                this.setState({data: result.data})
+    page = 0
+
+    onListScroll = (e) => {
+        const scrollDiv = e.currentTarget
+        console.log(`scrollDiv.clientHeight:${scrollDiv.clientHeight}
+                     scrollDiv.scrollHeight:${scrollDiv.scrollHeight}
+                     scrollDiv.scrollTop:${scrollDiv.scrollTop}
+                     scrollDiv.clientHeight + scrollDiv.scrollTop:${scrollDiv.clientHeight + scrollDiv.scrollTop}`)
+
+        if (scrollDiv.scrollHeight <= scrollDiv.clientHeight + scrollDiv.scrollTop + 20) {
+            this.getDataFromAPI()
+        }
+
+    }
+
+    getDataFromAPI = () => {
+
+        if(!this.state.isListLoading) {
+
+            this.setState({isListLoading: true})
+
+            fetch(`https://api.hh.ru/vacancies?page=${this.page}&per_page=50`)
+                .then(res => res.json())
+                .then((result) => {
+                    console.log(result)
+                    this.setState({data: {
+                        items: this.state.data.items.concat(result.items)}
+                      })
+                    this.setState({isListLoading: false})
+                this.page++
             }).catch((err) => {
+                this.setState({isListLoading: false})
                 console.error(err);
                 alert('something wrong')
-            })
+                })
+            }
+        }
+
+    componentDidMount() {
+        this.getDataFromAPI()
     }
 
   render() {
@@ -33,7 +69,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <div>
+          <div className={'list_wrapper'} onScroll={this.onListScroll}>
               {this.state.data.items.map( (item) => {
                   return <ListItem key={item.id} {...item}/>
               } )}
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const ListItem = (props) => {
-    return <h4>{props.name}
-               {props.employer.logo_urls ? <img src={props.employer.logo_urls[90]} alt={"logo of company"} /> : 'SOME DATA DOES NOT EXIST' }
-               {props.employer.name}
-               {props.salary ? props.salary.from : 'No information on salary'}
-           </h4>
+    return <div className={'cardItemVacancies'}>
+                <h4>
+                    <span className={'vacanci-name'}>
+                        {props.name}
+                    </span>
+                    <span className={'employer-logo'}>
+                        {props.employer.logo_urls ? <img src={props.employer.logo_urls[90]} alt={"logo of company"} /> : 'No information on logo' }
+                    </span>
+                    <span className={'employer-name'}>
+                        {props.employer.name}
+                    </span>
+                    <span className={'salary'}>
+                        {props.salary && props.salary.from ? props.salary.from : '---'}
+                    </span>
+                    <span className={'salary'}>
+                        {props.salary && props.salary.to ? props.salary.to : '---'}
+                    </span>
+                </h4>
+           </div>
 }
 
 export default ListItem;
