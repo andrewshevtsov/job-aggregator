9de7b3d874c26a4d2a40094cabfc791a66201b26
andrewshevtsov <andrewshevtsov@inbox.ru>
2018-11-19 22:43:49 +0300

    update styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ListItem from './ListItem';
 
@@ -57,12 +57,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React sample job parser</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
           <div className="list_container">
               <div className={'list_wrapper'} onScroll={this.onListScroll}>
                   {this.state.data.items.map( (item) => {
