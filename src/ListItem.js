dfe7589c1dde8ee8bfc786606b137955648bfa44
andrewshevtsov <andrewshevtsov@inbox.ru>
2019-06-13 22:46:46 +0300

    vacancies in 4 columns view was added

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
 const ListItem = (props) => {
+
     return <div className={'cardItemVacancies'}>
-                <p className={'employer-logo'}>
-                    {props.employer.logo_urls ? <img src={props.employer.logo_urls[90]} alt={"Company Logo"} /> : 'No logo' }
-                </p>
-                <p className={'vacancy-name'}>
+            <div className={'vacancy-inner'}>
+                <div className={'employer-logo'}>
+                    {props.employer.logo_urls ? <img src={props.employer.logo_urls[90]} alt={"Company Logo"} /> : 'No logo'}
+                </div>
+                <div className={'vacancy-name'}>
                     Вакансия: <span className="vacancy-text">{props.name}</span>
-                </p>
-                <p className={'employer-name'}>
+                </div>
+                <div className={'employer-name'}>
                     Компания: {props.employer.name}
-                </p>
-                <p className={'salary-min'}>
+                </div>
+                <div className={'salary-min'}>
                     Зарплата от: {props.salary && props.salary.from ? props.salary.from : '---'}
-                </p>
-                <p className={'salary-max'}>
+                </div>
+                <div className={'salary-max'}>
                     Зарплата до: {props.salary && props.salary.to ? props.salary.to : '---'}
-                </p>
-                <p className={'vacancy-city'}>
-                    Город: {props.address && props.address.city ? props.address.city : '---' }
-                </p>
-           </div>
+                </div>
+                <div className={'vacancy-city'}>
+                    Город: {props.address && props.address.city ? props.address.city : '---'}
+                </div>
+            </div>
+        </div>
 }
 
 export default ListItem;
